3eeb5f3173ea36d82164a9391fb7bb66ab566edb
ste-banks <stephen.banks4@nhs.net>
2026-04-27 15:49:42 +0100

    Update to set max-replicas to 1 - effectively stopping the container app - WIP

--- scripts/disaster-recovery/stop-dev-app-services.ps1
+++ scripts/disaster-recovery/stop-dev-app-services.ps1
@@ -32,7 +32,7 @@ if ($LASTEXITCODE -ne 0) {
   $LASTEXITCODE = 0
 }
 
-# Setting the min and max replicas to 0 to stop the container apps, as there is no direct stop command for container apps in Azure CLI
+# Setting the min and max replicas to 1 to effectively stop the container apps, as there is no direct stop command for container apps in Azure CLI
 foreach ($containerApp in $containerApps) {
   Write-Host "Stopping container app '$containerApp'"
 
@@ -40,7 +40,7 @@ foreach ($containerApp in $containerApps) {
     --name $containerApp `
     --resource-group $resourceGroup `
     --min-replicas 0 `
-    --max-replicas 0
+    --max-replicas 1
 
   if ($LASTEXITCODE -ne 0) {
     Write-Warning "Failed to stop Container App: $containerApp."

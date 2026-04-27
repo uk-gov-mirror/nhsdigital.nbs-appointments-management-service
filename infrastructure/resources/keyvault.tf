46041e04cf00151cad7da7c62a11b2f4ca182688
Dave-O-NHS <david.olsavsky1@nhs.net>
2026-04-27 14:49:56 +0100

    creating keyvault and allowing PITR to set active db name

---
+++ infrastructure/resources/keyvault.tf
@@ -0,0 +1,29 @@
+data "azurerm_client_config" "current" {}
+
+# Create the Key Vault
+resource "azurerm_key_vault" "nbs_mya_key_vault" {
+  name                        = "${var.application}-kv-${var.environment}-${var.loc}"
+  location                    = var.location
+  resource_group_name         = local.resource_group_name
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  sku_name                    = "standard"
+  
+  rbac_authorization_enabled   = true
+  purge_protection_enabled    = true
+}
+
+resource "azurerm_key_vault_secret" "active_cosmos_account" {
+  name         = "ActiveCosmosDBAccountName"
+  value        = "${var.application}-cdb-${var.environment}-${var.loc}" 
+  key_vault_id = azurerm_key_vault.nbs_mya_key_vault.id
+
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+resource "azurerm_role_assignment" "pipeline_secret_access" {
+  scope                = azurerm_key_vault.nbs_mya_key_vault.id
+  role_definition_name = "Key Vault Secrets Officer"
+  principal_id         = data.azurerm_client_config.current.object_id
+}

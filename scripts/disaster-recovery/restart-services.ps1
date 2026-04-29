6471f8c9d3e6a9e86e84e292538e8a0954d079d1
ste-banks <stephen.banks4@nhs.net>
2026-04-29 15:41:44 +0100

    Adding script to activate revisions within the container apps that were deactivate during PITR process

--- scripts/disaster-recovery/restart-services.ps1
+++ scripts/disaster-recovery/restart-services.ps1
@@ -1,21 +1,52 @@
 param (
-    [Parameter(Mandatory=$true)]
-    [string]$ResourceGroup
+  [string][Parameter(Mandatory)]$resourceGroup,
+  [string][Parameter(Mandatory)]$region
 )
 
 # Start the Web App
-$webApp = "vaccs-mya-app-dev-uks"
+$webApp = "vaccs-mya-app-dev-$region"
 Write-Host "Starting Web App: $webApp in $ResourceGroup..."
 az webapp start --name $webApp --resource-group $ResourceGroup
 
 # Start the Function Apps (Excluding hlfunc)
 $functions = @(
-    "vaccs-mya-func-dev-uks",
-    "vaccs-mya-sbfunc-dev-uks",
-    "vaccs-mya-timerfunc-dev-uks"
+    "vaccs-mya-func-dev-$region",
+    "vaccs-mya-sbfunc-dev-$region",
+    "vaccs-mya-timerfunc-dev-$region"
 )
 
 foreach ($app in $functions) {
     Write-Host "Starting Function App: $app in $ResourceGroup..."
     az functionapp start --name $app --resource-group $ResourceGroup
+}
+
+$containerApps = @("vaccs-mya-aggregator-dev-$region", "vaccs-mya-auditor-dev-$region")
+
+foreach ($containerApp in $containerApps) {
+  Write-Host "Restarting inactive revisions in container app '$containerApp'"
+
+  $revisions = @(az containerapp revision list `
+    --name $containerApp `
+    --resource-group $resourceGroup `
+    --query "[?!properties.active].name" `
+    -o tsv)
+
+  if (-not $revisions) {
+    Write-Host "No inactive revisions found for $containerApp"
+    continue
+  }
+
+  foreach ($rev in $revisions) {
+    Write-Host "Reactivating revision '$rev'"
+
+    az containerapp revision activate `
+      --name $containerApp `
+      --resource-group $resourceGroup `
+      --revision $rev
+
+    if ($LASTEXITCODE -ne 0) {
+      Write-Warning "Failed to activate revision '$rev' for '$containerApp'"
+      $LASTEXITCODE = 0
+    }
+  }
 }

9494d84b92662d9c3aed497f4ee7608861cbe5f8
Dave-O-NHS <david.olsavsky1@nhs.net>
2026-04-29 12:18:17 +0100

    fixing key vault creation

--- infrastructure/resources/keyvault.tf
+++ infrastructure/resources/keyvault.tf
@@ -2,21 +2,25 @@ data "azurerm_client_config" "current" {}
 
 # Create the Key Vault
 resource "azurerm_key_vault" "nbs_mya_key_vault" {
-  name                        = "${var.application}-kv-${var.environment}-${var.loc}"
-  location                    = var.location
-  resource_group_name         = local.resource_group_name
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  sku_name                    = "standard"
-  
-  enable_rbac_authorization   = true
-  purge_protection_enabled    = true
+  name                = "${var.application}-kv-${var.environment}-${var.loc}"
+  location            = var.location
+  resource_group_name = local.resource_group_name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "standard"
+
+  enable_rbac_authorization = true
+  purge_protection_enabled  = true
 }
 
 resource "azurerm_key_vault_secret" "active_cosmos_account" {
   name         = "ActiveCosmosDBAccountName"
-  value        = "${var.application}-cdb-${var.environment}-${var.loc}" 
+  value        = "${var.application}-cdb-${var.environment}-${var.loc}"
   key_vault_id = azurerm_key_vault.nbs_mya_key_vault.id
 
+  depends_on = [
+    azurerm_role_assignment.pipeline_secret_access
+  ]
+  
   lifecycle {
     ignore_changes = [value]
   }

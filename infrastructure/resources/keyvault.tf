d8151fd7a80102446187882da87301480e774a49
Dave-O-NHS <david.olsavsky1@nhs.net>
2026-04-29 11:38:24 +0100

    changing parameter name for enabling rbac on key vault

--- infrastructure/resources/keyvault.tf
+++ infrastructure/resources/keyvault.tf
@@ -8,7 +8,7 @@ resource "azurerm_key_vault" "nbs_mya_key_vault" {
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   sku_name                    = "standard"
   
-  rbac_authorization_enabled   = true
+  enable_rbac_authorization   = true
   purge_protection_enabled    = true
 }
 

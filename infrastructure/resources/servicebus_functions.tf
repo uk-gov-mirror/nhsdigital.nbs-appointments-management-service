1a150b3c5d45cb5a882d88a0b81f328bfe3f4ed8
jsed-nhs <193929923+jsed-nhs@users.noreply.github.com>
2026-04-30 14:56:44 +0100

    Add CreateRecurrenceFunction disabled to the 3 non-needed function apps

--- infrastructure/resources/servicebus_functions.tf
+++ infrastructure/resources/servicebus_functions.tf
@@ -124,6 +124,7 @@ resource "azurerm_windows_function_app" "nbs_mya_service_bus_func_app" {
     "AzureWebJobs.AggregateDailySiteSummary.Disabled"                      = var.aggregator_changefeed_enable
     "AzureWebJobs.ProposeCancelDateRangeFunction.Disabled"                 = true
     "AzureWebJobs.CancelDateRangeFunction.Disabled"                        = true
+    "AzureWebJobs.CreateRecurrenceFunction.Disabled"                       = true
   }
 
   sticky_settings {
@@ -258,6 +259,7 @@ resource "azurerm_windows_function_app_slot" "nbs_mya_service_bus_func_app_previ
     "AzureWebJobs.GetSiteUsersReportFunction.Disabled"                     = true
     "AzureWebJobs.ProposeCancelDateRangeFunction.Disabled"                 = true
     "AzureWebJobs.CancelDateRangeFunction.Disabled"                        = true
+    "AzureWebJobs.CreateRecurrenceFunction.Disabled"                       = true
   }
 
   identity {

aeee702dc30b480eb2ed4a02c26c912da7a64662
Dave-O-NHS <david.olsavsky1@nhs.net>
2026-05-01 10:46:40 +0100

    adding role assignment for new key vault

--- infrastructure/resources/keyvault.tf
+++ infrastructure/resources/keyvault.tf
@@ -31,3 +31,29 @@ resource "azurerm_role_assignment" "pipeline_secret_access" {
   role_definition_name = "Key Vault Secrets Officer"
   principal_id         = data.azurerm_client_config.current.object_id
 }
+
+
+locals {
+  mya_principals = [
+    azurerm_container_app_job.nbs_mya_booking_extracts_job.identity[0].principal_id,
+    azurerm_container_app_job.nbs_mya_capacity_extracts_job.identity[0].principal_id,
+    azurerm_container_app.nbs_mya_auditor.identity[0].principal_id,
+    azurerm_container_app.nbs_mya_aggregator.identity[0].principal_id,
+
+    azurerm_windows_function_app.nbs_mya_high_load_func_app.identity[0].principal_id,
+    azurerm_windows_function_app_slot.nbs_mya_high_load_func_app_preview.identity[0].principal_id,
+    azurerm_windows_function_app.nbs_mya_http_func_app.identity[0].principal_id,
+    azurerm_windows_function_app_slot.nbs_mya_http_func_app_preview.identity[0].principal_id,
+    azurerm_windows_function_app.nbs_mya_service_bus_func_app.identity[0].principal_id,
+    azurerm_windows_function_app_slot.nbs_mya_service_bus_func_app_preview.identity[0].principal_id,
+    azurerm_windows_function_app.nbs_mya_timer_func_app.identity[0].principal_id,
+    azurerm_windows_function_app_slot.nbs_mya_timer_func_app_preview.identity[0].principal_id,
+  ]
+}
+
+resource "azurerm_role_assignment" "vault_access" {
+  for_each             = toset(local.mya_principals)
+  scope                = azurerm_key_vault.nbs_mya_key_vault.id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = each.value
+}

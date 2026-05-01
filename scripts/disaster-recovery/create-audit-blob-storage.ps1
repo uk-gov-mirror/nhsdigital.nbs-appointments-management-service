5b1cf01f6305eaa3a71902721b1b0af502b70196
ste-banks <stephen.banks4@nhs.net>
2026-05-01 10:52:29 +0100

    Formatting change - also adding error

--- scripts/disaster-recovery/create-audit-blob-storage.ps1
+++ scripts/disaster-recovery/create-audit-blob-storage.ps1
@@ -4,10 +4,14 @@ param (
 [string][Parameter(Mandatory)]$subscriptionId
 )
 
-az storage account create `
-    --name $blobAccountName `
-    --resource-group $resourceGroup `
-    --subscription $subscriptionId `
-    --location uksouth `
-    --sku Standard_LRS `
-    --min-tls-version TLS1_2
+az storage account create
+--name $blobAccountName `
+--resource-group $resourceGroup `
+--subscription $subscriptionId `
+--location uksouth `
+--sku Standard_LRS `
+--min-tls-version TLS1_2
+
+if ($LASTEXITCODE -ne 0) {
+  throw "Failed to create new storage account '$blobAccountName' in '$resourceGroup'"
+}
